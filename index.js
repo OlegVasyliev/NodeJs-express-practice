f4b8a693b87175c986346e1724091ca7d34c04b4
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2020-11-03 22:11:23 +0200

    added data render

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: "Main page",
+    isHome: true
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('about')
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: "Add course",
+    isAdd: true
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: "Courses",
+    isCourses: true
+  })
 })
 
 
